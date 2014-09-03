a63cb71b17113f44ea00d23ac16c37d88f780448
Tom Hartman <thomas.lees.hartman@gmail.com>
2014-09-03 10:50:25 -0400

    Added some default makefile.am and configure.ac scripts as well as associated functions to support their creation.

--- atu.el
+++ atu.el
@@ -20,6 +20,55 @@
 ;;; Code:
 
 (require 'f)
+(require 'ht)
+
+(defvar *default-makefile.am*
+  "AUTOMAKE_OPTIONS = 1.4
+ACLOCAL_AMFLAGS = -I config
+
+SUBDIRS =
+
+EXTRA_DIST = BUGS INSTALL-unix $(m4sources)")
+
+(defvar *default-configure.ac*
+"dnl Process this file with autoconf to produce a configure script.
+
+AC_PREREQ(2.26)
+
+m4_define([%project-name%_major_version], [0])
+m4_define([%project-name%_minor_version], [1])
+m4_define([%project-name%_version], \\
+          [%project-name%_major_version.%project-name%_minor_version])
+
+AC_INIT([%project-name%],[1.0])
+AC_CONFIG_MACRO_DIR([config])
+AM_INIT_AUTOMAKE([1.11 dist-bzip2])
+LT_PREREQ([2.2])
+LT_INIT([dlopen])
+
+AC_SUBST(%PROJECT-NAME%_MAJOR_VERSION, [%project-name%_major_version])
+AC_SUBST(%PROJECT-NAME%_MINOR_VERSION, [%project-name%_minor_version])
+AC_SUBST(%PROJECT-NAME%_VERSION, [%project-name%_version])
+
+dnl Check for programs
+
+AC_CONFIG_FILES([Makefile])
+
+AC_OUTPUT")
+
+(defun atu-generate-default-makefile.am (dir)
+  (interactive "DProject root: ")
+  (when (f-exists? (f-join dir "Makefile.am"))
+    (error "%s already exists, will not clobber" (f-join dir "Makefile.am")))
+  (f-write-text *default-makefile.am* 'utf-8 (f-join dir "Makefile.am")))
+
+(defun atu-generate-default-configure.ac (dir project-name)
+  (interactive "DProject root: \nsProject name: ")
+  (when (f-exists? (f-join dir "configure.ac"))
+    (error "%s already exists, will not clobber" (f-join dir "configure.ac")))
+  (f-write-text (atu-format *default-configure.ac*
+                            (ht (:project-name project-name)))
+                'utf-8 "configure.ac"))
 
 (defun atu-add-makefile.am-subdir (subdir makefile.am)
   "Add SUBDIR to the list of 'SUBDIRS' values in MAKEFILE.AM"
@@ -50,6 +99,19 @@
                          (format "AC_CONFIG_FILES([%s/src/Makefile])\n" subdir)
                          (substring text i))
                  'utf-8 configure.ac)))))
+
+(defun atu-format (str test-suite)
+  "Scan the STR for %*% and replace with the hash value associated in TEST-SUITE."
+  (let ((retval str))
+    (maphash #'(lambda (key val)
+                 (setf str (replace-regexp-in-string
+                            (concat "%" (substring (symbol-name key) 1) "%") val str)))
+             test-suite)
+    str))
+
+(defun atu-autoreconf (path)
+  "Run autoreconf -i from the PATH."
+  (start-shell-process))
 
 (provide 'atu)
 
